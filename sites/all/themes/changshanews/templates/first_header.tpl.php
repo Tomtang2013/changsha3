9b6cc5e096fcb576c6823b716eb583ad4c70d610
Tom tang <tom.tang@perficientgdc.com.cn>
2014-02-27 16:55:09 +0800

    change css

--- sites/all/themes/changshanews/templates/first_header.tpl.php
+++ sites/all/themes/changshanews/templates/first_header.tpl.php
@@ -18,17 +18,23 @@ $news = $base_path .'news/summary';
         });
     });
 </script>
+<!--[if IE 8]>
+<style>
+    .header_left{ float:left;}
+    .header_right{ float:right;}
+</style>
+<![endif]-->
 
 <div class="first_header ">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-sm-6">
+                <div class="col-md-6 col-sm-6 header_left" >
                     <ul style="margin-top:20px;">
                         <li> <a href="<?php print $base_path?>">设计长沙</a></li>
                         <li> &nbsp;|&nbsp;<a href="<?php print $base_path?>">潇湘晨报品牌创意中心</a></li>
                     </ul>
                 </div>
-                <div class="col-md-6 col-sm-6">
+                <div class=" col-md-6 col-md-6 header_right"  >
                      <ul class="nav_link" >
                         <li > <a  style="padding-left:12px;" href="<?php print $about_company_path?>">关于</a></li>
                         <li><a  style="padding-left:12px;" href="<?php print $news?>">动态</a> </li>
