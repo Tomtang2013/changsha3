f70230cef6af3909bced938ffbf0d16d0e0caa59
unknown <tom.tang@H2400-0360L.perficient.com>
2014-06-09 16:26:09 +0800

    add nav img

--- sites/all/themes/changshanews/templates/designvote/vote_main.tpl.php
+++ sites/all/themes/changshanews/templates/designvote/vote_main.tpl.php
@@ -1,20 +1,29 @@
-<?php ?>
+<?php 
+ global $base_path;
+ $theme_path = $base_path.drupal_get_path('theme', 'changshanews');
+?>
 <script  type="text/javascript">
     jQuery(function(){
-//        jQuery('#vote_ul').localScroll(0);
-
-    
-
-        hideTab();
+		hideTab();
         showTabByIdx('#tab1');
         jQuery('.vote_design_main_li').each(function(){
             jQuery(this).find('a').click(function(){
                 var href = jQuery(this).attr('href');
-                 hideTab();
-                 showTabByIdx(href);
-                 jQuery('html, body').animate({
+                hideTab();
+                showTabByIdx(href);
+                jQuery('html, body').animate({
                     scrollTop: 650
                 }, 0.1);
+				jQuery('.vote_design_main_li').find('a').find('img').each(function(){
+					var path = jQuery(this).attr('src');
+					path = path.replace(/1/, "2");
+					jQuery(this).attr('src',path);
+				});
+				
+				var img_path = jQuery(this).find('img').attr('src');
+				img_path = img_path.replace(/2/, "1");
+				jQuery(this).find('img').attr('src',img_path);
+				
             });
         });
     });
@@ -33,15 +42,15 @@
         <div class="row">
             <div class="col-md-3 col-sm-3">
                 <ul style="float:right; width: 100%;" id="vote_ul">
-                    <li class="vote_design_main_li"><a href="#tab1">活动概括</a></li>
-                    <li class="vote_design_main_li"><a href="#tab2">参赛方式</a></li>
-                    <li class="vote_design_main_li"><a href="#tab3">大赛评委</a></li>
-                    <li class="vote_design_main_li"><a href="#tab4">奖品设置</a></li>
-                    <li class="vote_design_main_li"><a href="#tab5">作品展示</a></li>
-                    <li class="vote_design_main_li"><a href="#tab6">往届回顾</a></li>
+                    <li class="vote_design_main_li"><a href="#tab1"><img src="<?php print $theme_path;?>/images/designvote/a2.png" alt=""/></a></li>
+                    <li class="vote_design_main_li"><a href="#tab2"><img src="<?php print $theme_path;?>/images/designvote/b2.png" alt=""/></a></li>
+                    <li class="vote_design_main_li"><a href="#tab3"><img src="<?php print $theme_path;?>/images/designvote/c2.png" alt=""/></a></li>
+                    <li class="vote_design_main_li"><a href="#tab4"><img src="<?php print $theme_path;?>/images/designvote/d2.png" alt=""/></a></li>
+                    <li class="vote_design_main_li"><a href="#tab5"><img src="<?php print $theme_path;?>/images/designvote/e2.png" alt=""/></a></li>
+                    <li class="vote_design_main_li"><a href="#tab6"><img src="<?php print $theme_path;?>/images/designvote/f2.png" alt=""/></a></li>
                 </ul>
                 <ul style="float:right; width: 100%;">
-                    <li class="vote_design_main_li">下载专区</li>
+                    <li class="vote_design_main_li"><img src="<?php print $theme_path;?>/images/designvote/g2.png" alt=""/></li>
                 </ul>
             </div>
             <div class="col-md-9 col-sm-9" id="design_vote_content">
